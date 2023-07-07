fc5580148efbe8889240b1d3bcd69dee3c132015
antonifritz <256763@student.pwr.edu.pl>
2023-07-07 13:56:47 +0200

    adding files

---
+++ lab6/zad5.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Systemy operacyjne 2 – laboratorium nr 6
+#
+# Celem zajęć jest zapoznanie się z możliwościami narzędzia awk.
+#
+# Tradycyjnie nie przywiązujemy zbyt dużej wagi do środowiska roboczego.
+# Zakładamy, że format i układ danych w podanych plikach nie ulega zmianie,
+# ale same wartości, inne niż podane wprost w treści zadań, mogą ulec zmianie,
+# a przygotowane zadania wciąż powinny działać poprawnie (robić to, co trzeba).
+#
+# Wszystkie chwyty dozwolone, ale w wyniku ma powstać tylko to, o czym jest
+# mowa w treści zadania – nie generujemy żadnych dodatkowych plików pośrednich.
+# Większość rozwiązań powinno sprowadzić się do jednego, zmyślnego wywołania
+# programu `awk` z odpowiednimi argumentami.
+#
+
+#
+# Zadanie 5.
+# Plik `dodatkowe/nowomowa.txt` zawiera przykładowy tekst z generatora
+# http://lipsum.pl/, pozbawiony polskich znaków (dla ułatwienia pracy).
+# Proszę przygotować własną wariację narzędzia `wc`, czyli zliczyć
+# przy pomocy programu awk ile jest znaków, słów i linii w podanym pliku
+# wejściowym. Każdą wartość (tylko liczby!) wpisać w nowej linii.
+# Uwaga! Proszę pamiętać o uwzględnieniu znaków końca linii w obliczeniach
+# dotyczących liczby znaków!
+#
+
+awk 'BEGIN{FS = " "; SIGN_NUM = 0; WORD_NUM = 0;}
+{SIGN_NUM = SIGN_NUM + length($0) + 1; WORD_NUM = WORD_NUM + NF;} 
+END{print SIGN_NUM "\n" WORD_NUM "\n" NR}' dodatkowe/nowomowa.txt
